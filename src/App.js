80e6657f6d66c658dd4ab041190b0c6c97dc9c45
staffanholmnas <staffan.holmnas@centria.fi>
2020-09-23 21:55:30 +0300

    Add effect hook to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true) 
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
@@ -24,16 +37,16 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
-  const notesToShow = showAll 
-  ? notes
-  : notes.filter(note => note.important)
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
@@ -42,9 +55,9 @@ const App = (props) => {
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input 
-        value={newNote} 
-        onChange={handleNoteChange}/>
+        <input
+          value={newNote}
+          onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
     </div>
