ee216d57dd485a4e24bd9bb269392f08dae4410f
staffanholmnas <staffan.holmnas@centria.fi>
2020-09-23 22:29:47 +0300

    Edit app.js

--- src/App.js
+++ src/App.js
@@ -19,17 +19,20 @@ const App = () => {
 
   console.log('render', notes.length, 'notes')
 
-  const addNote = (event) => {
+  const addNote = event => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
+      date: new Date(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
   }
 
   const handleNoteChange = (event) => {
