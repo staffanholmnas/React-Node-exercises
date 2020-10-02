68759c158547abb2ac90996f04a76f230d49b638
staffanholmnas <staffan.holmnas@centria.fi>
2020-10-02 18:06:23 +0300

    Edit index

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,15 @@ let notes = [
     }
 ]
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+app.use(requestLogger)
+
 app.get('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
@@ -47,31 +56,38 @@ app.delete('/api/notes/:id', (request, response) => {
 
 const generateId = () => {
     const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
     return maxId + 1
-  }
-  
-  app.post('/api/notes', (request, response) => {
+}
+
+app.post('/api/notes', (request, response) => {
     const body = request.body
-  
+
     if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+        return response.status(400).json({
+            error: 'content missing'
+        })
     }
-  
+
     const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
     }
-  
+
     notes = notes.concat(note)
-  
+
     response.json(note)
-  })
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 
 const PORT = 3001
 app.listen(PORT, () => {
