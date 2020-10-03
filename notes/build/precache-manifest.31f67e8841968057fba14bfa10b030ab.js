43a337097cb37d415fb1f1609e0009a56ab85d2d
staffanholmnas <staffan.holmnas@centria.fi>
2020-10-03 20:38:59 +0300

    Run build, copy to root

---
+++ notes/build/precache-manifest.31f67e8841968057fba14bfa10b030ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbe51ae0a0ec14f26d438d368d564a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca230533fdedb3c111fb",
+    "url": "/static/css/main.99313a8e.chunk.css"
+  },
+  {
+    "revision": "b687281982d0621163a2",
+    "url": "/static/js/2.590b4e07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.590b4e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca230533fdedb3c111fb",
+    "url": "/static/js/main.4543e011.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
