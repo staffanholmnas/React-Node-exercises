af22f339a85158c33f53c4b70d15b96bb3e0c409
staffanholmnas <staffan.holmnas@centria.fi>
2020-10-03 21:01:25 +0300

    push to heroku

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ let notes = [
         content: "GET and POST are the most important methods of HTTP protocol",
         date: "2019-05-30T19:20:14.298Z",
         important: true
+    },
+    {
+        id: 4,
+        content: "I wish I had eyes",
+        date: "2019-05-30T19:20:14.298Z",
+        important: true
     }
 ]
 
